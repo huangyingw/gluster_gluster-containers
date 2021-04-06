162ef9b8d5bbc2b17fbdf81ade3c3f752096b3c9
Ying Huang <huangyingw@gmail.com>
2021-04-05 22:30:10 -0700

    n

--- run.sh
+++ run.sh
@@ -4,3 +4,14 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 docker build -t gluster-centos CentOS
+
+docker run \
+    -v ./etc/glusterfs/:/etc/glusterfs:z \
+    -v /var/lib/glusterd/:/var/lib/glusterd:z \
+    -v /var/log/glusterfs/:/var/log/glusterfs:z \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    -v /dev/:/dev \
+    -d \
+    --privileged=true \
+    --net=host \
+    gluster/gluster-centos
